c0388dee650d016efccae6da0eb070419e70a4f2
sourabh1428 <sppathak1428@gmail.com>
2023-11-15 00:17:49 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,27 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
+import Welcome from './welcome'
 
+
+export default function App() {
+  let[name,setName]=useState('');
+  let[change,setChange]=useState('');
+  let[show,setShow]=useState(false);
+  function handleChange(event){
+    setChange(event.target.value);
+  }
+  function handleClick(event){
+    setName(change);
+    setShow(1);
+  }
+
+
+    return (
+    <div>
+        <input type="text" value={change} onChange={handleChange}/>
+        <button onClick={handleClick}>Login</button>
+        {show?(<Welcome name={name}/>):null}
+
+
+    </div>
+  )
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Welcome({name}) {
+  return (
+    <div>
+        <h1>WELCOME {name}</h1>
+        <h2>Welcome to School</h2>
+    </div>
+  )
+}
